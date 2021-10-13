83c05f75bcc302752a32dbc3d4e0d8f54a489b4c
icelava <icelava@gmail.com>
2021-10-13 17:34:23 +0800

    .NET solution with base ASP.NET (MVC) web site.

---
+++ XRayFrontWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace XRayFrontWeb.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
