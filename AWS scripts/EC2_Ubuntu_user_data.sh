863f23b4a5258440630ee46581cdd915e3e62a23
icelava <icelava@gmail.com>
2021-10-19 19:26:43 +0800

    Update user data script to download and install X-Ray daemon.

--- AWS scripts/EC2_Ubuntu_user_data.sh
+++ AWS scripts/EC2_Ubuntu_user_data.sh
@@ -8,3 +8,7 @@ sudo apt-get update; \
   sudo apt-get install -y apt-transport-https && \
   sudo apt-get update && \
   sudo apt-get install -y dotnet-sdk-3.1
+
+  BUCKETURL=https://s3.ap-southeast-1.amazonaws.com/aws-xray-assets.ap-southeast-1
+  wget $BUCKETURL/xray-daemon/aws-xray-daemon-3.x.deb
+  sudo dpkg -i aws-xray-daemon-3.x.deb
