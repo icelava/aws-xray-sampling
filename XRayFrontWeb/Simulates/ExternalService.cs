661261cfaffa974d4eea1fd4a8be9b82b58bf84d
icelava <icelava@gmail.com>
2021-10-20 17:22:06 +0800

    If processing is delayed, only end subsegment AFTER asynchronous action complete.

--- XRayFrontWeb/Simulates/ExternalService.cs
+++ XRayFrontWeb/Simulates/ExternalService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Amazon.XRay.Recorder.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,9 +10,21 @@ namespace XRayFrontWeb.Simulates
     {
 		public static async Task<int> DelayRandomly()
 		{
+			AWSXRayRecorder.Instance.BeginSubsegment("DelayRandomlyService");
 			var delayedTime = (new Random()).Next(1500);
-			if (delayedTime < 0) await Task.Delay(delayedTime);
+			// If no delay then exit immediately without delay action.
+			if (delayedTime == 0)
+			{
+				AWSXRayRecorder.Instance.EndSubsegment();
+				return delayedTime;
+			}
 
+			await Task.Delay(delayedTime).ContinueWith(t =>
+			{
+				AWSXRayRecorder.Instance.EndSubsegment();
+			});
+
+			
 			return delayedTime;
 		}
     }
