e34489ad257e94e0ae390b2a8fd0621d3c00095b
icelava <icelava@gmail.com>
2021-10-25 15:25:28 +0800

    Test controller with random-delay response. Include same AWS configuration class.

---
+++ XRayBackApi/Configuration/AwsConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XRayBackApi.Configuration
+{
+	public class AwsConfig
+	{
+		public static AwsConfig Instance;
+		public string Account { get; set; }
+		public string Region { get; set; }
+		public string AccessKeyId { get; set; }
+		public string SecretAccessKey { get; set; }
+		public string S3Bucket { get; set; }
+
+
+	}
+}
---
+++ XRayBackApi/Controllers/TierController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace XRayBackApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TierController : ControllerBase
+	{
+		// GET: api/<TierController>
+		[HttpGet]
+		public async Task<int> Get()
+		{
+			var delayedTime = (new Random()).Next(1500);
+			if (delayedTime == 0) return delayedTime;
+
+			await Task.Delay(delayedTime);
+
+			return delayedTime;
+		}
+
+		// GET api/<TierController>/5
+		[HttpGet("{id}")]
+		public string Get(int id)
+		{
+			return "value";
+		}
+
+		// POST api/<TierController>
+		[HttpPost]
+		public void Post([FromBody] string value)
+		{
+		}
+
+		// PUT api/<TierController>/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody] string value)
+		{
+		}
+
+		// DELETE api/<TierController>/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+	}
+}
